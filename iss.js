91e16f3e423543dcfaadea2f0379bcc52c2d9e50
habibcodes <habib.codes@gmail.com>
2021-09-23 19:17:23 -0400

    added logic for fetching geo-coordinates based on provided IP data

--- iss.js
+++ iss.js
@@ -32,12 +32,40 @@ const fetchMyIP = (callback) => {
     console.log('body:', body); // Print the HTML for the Google homepage.
 
     const ip = JSON.parse(body).ip;
-    console.log(ip);
+    // send as callback
+    callback(null, ip);
   });
 };
 
 
-module.exports = { fetchMyIP };
 // fetch geo coordinates (Lat and Long) for our IP
+const fetchCoordsByIP = (ip, callback) => {
+  request('https://freegeoip.app/json/99.232.103.31', (error, response, body) => {
+    // non status errors
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinats for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    // parse data
+    const {latitude, longitude} = JSON.parse(body);
+    // send as callback
+    callback(null, {latitude, longitude});
+  });
+};
+
+
+
 // fetch next ISS flyovers for our geo coordinates
 
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+};
